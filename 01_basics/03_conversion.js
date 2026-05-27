cd30f5576375ae4f3ca3188e94b99971ce70761f
Avishek-bin <87655381+Avishek-bin@users.noreply.github.com>
2026-05-27 07:11:02 +0000

    changes 03

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -6,3 +6,17 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 
 console.log(typeof valueInNumber);
+
+
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+console.log("1" + 2)
+
+console.log(1 + "2")
