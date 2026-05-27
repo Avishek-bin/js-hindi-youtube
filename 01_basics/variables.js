99896446bdc3e7345c67ce22f3fc9c0c44ba43be
Avishek-bin <87655381+Avishek-bin@users.noreply.github.com>
2026-05-27 06:46:52 +0000

    02 ans 03

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "avshek@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+
+//accountId = 2 ->not allowed
+
+accountEmail = "hch@kh.com"
+accountPassword = "123223"
+accountCity = "Gurgaon"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
